05260677a87eac764c3069c559679b7201c9bff4
Ezhixuan <ezhixuanna@163.com>
2025-03-26 01:05:01 +0800

    Initial commit

---
+++ src/main/java/com/ezhixuan/xuanblog_backend/XuanBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ezhixuan.xuanblog_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuanBlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XuanBlogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezhixuan/xuanblog_backend/redis/RedisTest.java
@@ -0,0 +1,4 @@
+package com.ezhixuan.xuanblog_backend.redis;
+
+public class RedisTest {
+}
