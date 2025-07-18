26bacd3a9392ec4fdc8e9b5681a724b44534f525
Ezhixuan <ezhixuanna@163.com>
2025-07-18 16:39:11 +0800

    refactor(pom): 重构项目模块并更新依赖
    - 将 blog_admin 模块重命名为 blog_server
    - 更新版本号为0.0.1-beat- 添加 spring-boot-maven-plugin插件配置
    - 修改 blog_common 模块中的 Redis 依赖- 删除 blog_test 模块- 更新根 pom.xml 文件
    - 删除与 Redisson 相关的配置类和属性类
    - 删除 Redis 测试类和测试应用

--- blog_test/src/main/java/com/ezhixuan/blog/TestApplication.java
+++
@@ -1,9 +0,0 @@
-package com.ezhixuan.blog;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class TestApplication {
-    public static void main(String[] args) {
-    }
-}
